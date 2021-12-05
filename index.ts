41143fc804da7776fb1d1b10126060507b853804
asiedusolo <solomon.asiedu@amalitech.org>
2021-12-05 09:44:13 -0500

    adding additional information for receipt

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import products from './products';
 
-let productName: string = 'shirt';
+let productName: string = 'beanie';
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
 
 const product = products.filter(product => product.name === productName)[0];
 console.log(product)
 
 if(product.preOrder === 'true'){
-    console.log('We will send you a message when its on its way')
-  }
+  console.log('We will send you a message when its on its way')
+}

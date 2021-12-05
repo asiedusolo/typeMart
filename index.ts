87996ec735ec85225be71c9d85de4f81ef9f653e
asiedusolo <solomon.asiedu@amalitech.org>
2021-12-05 09:49:58 -0500

    adding logic to exempt products priced 25 and above from shipping cost

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
 import products from './products';
 
-let productName: string = 'beanie';
+let productName: string = 'shirt';
 let shipping: number;
 let taxPercent: number;
 let taxTotal: number;
 let total: number;
+let shippingAddress: string = 'House no 81/4, Akosua Ave. Street';
 
 const product = products.filter(product => product.name === productName)[0];
 console.log(product)
 
 if(product.preOrder === 'true'){
   console.log('We will send you a message when its on its way')
+}
+
+if(Number(product.price) >= 25){
+  shipping = 0
+  console.log('We provide free shipping for this product')
+}else{
+  shipping = 5
 }
